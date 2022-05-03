651bc0696b3440a7b4971716914b955c516851d2
Zilvinas Marozas <zmarozas@gmail.com>
2022-05-03 15:55:41 -0400

    changes from lesson

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,33 @@
 import "../css/App.css";
+import ListContacts from "./ListContacts";
 
 const App = () => {
-  return <div>Hello World</div>;
+  const contacts = [
+    {
+      id: "karen",
+      name: "Karen Isgrigg",
+      handle: "karen_isgrigg",
+      avatarURL: "http://localhost:5001/karen.jpg",
+    },
+    {
+      id: "richard",
+      name: "Richard Kalehoff",
+      handle: "richardkalehoff",
+      avatarURL: "http://localhost:5001/richard.jpg",
+    },
+    {
+      id: "tyler",
+      name: "Tyler McGinnis",
+      handle: "tylermcginnis",
+      avatarURL: "http://localhost:5001/tyler.jpg",
+    },
+  ];
+
+  return (
+    <div>
+      <ListContacts contacts={contacts}/>
+    </div>
+  );
 };
 
 export default App;
---
+++ src/components/ListContacts.js
@@ -0,0 +1,30 @@
+const ListContacts=({contacts})=>{
+    return (<ol className="contact-list">
+        {
+            contacts.map((contact)=>{
+                return (
+                    <li key={contact.id} className="contact-list-item">{contact.name}
+                    <div className="contact-avatar" style={{
+                        backgroundImage: `url(${contact.avatarURL})`
+                    }}>
+                    </div>
+
+                    <div className="contact-details">
+                        <p>{contact.name}</p>
+                        <p>{contact.handle}</p>
+                    </div>
+                    <button
+                      className="contact-remove"
+                    >Remove</button>
+                    </li>
+                );
+            })
+        }
+
+    </ol>
+    );
+        
+
+}
+
+export default ListContacts;
